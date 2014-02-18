noblehng/glibc

commit 6f5c31171fbd45c79c9bbeea4d6305f9c798e421

* resolv/res_init.c (__res_vinit): Fill in IPv4 name server
	information at the correct index.

resolv/res_init.c
@@ -314,9 +314,9 @@ __res_vinit(res_state statp, int preinit) {
 			cp++;
 		    if ((*cp != '\0') && (*cp != '\n')
 			&& __inet_aton(cp, &a)) {
-			statp->nsaddr_list[nserv].sin_addr = a;
-			statp->nsaddr_list[nserv].sin_family = AF_INET;
-			statp->nsaddr_list[nserv].sin_port =
+			statp->nsaddr_list[nservall].sin_addr = a;
+			statp->nsaddr_list[nservall].sin_family = AF_INET;
+			statp->nsaddr_list[nservall].sin_port =
 				htons(NAMESERVER_PORT);
 			nserv++;
 #ifdef _LIBC

commit dbb303e20b2094e0dc0d72c9ea62ed6279a12bcb

resolv/res_init.c: Correctly index and count IPv4 nameserver entries.
IPv4 nameserver entries was incorrectly index in statp->nsaddr_list[],
which was introduced by commit 6f5c31171fbd, and cause all IPv4 nameserver
entries after an IPv6 nameserver entry from resolv.conf being ignored by
resolver.

For example, if there are three nameserver entries in resolv.conf, and
order is IPv6 IPv6 IPv4, then in resolv/res_send.c, EXT(statp).nsmap[]
will be MAXNS+1 MAXNS+1 0, thus in L443 of resolv/res_send.c &statp->
nsaddr_list[0] is supposed to be copy to EXT(statp).nsaddrs[2]. But
because statp->nsaddr_list[] was index by nservall, which means the
IPv4 nameserver address will be in &statp->nsaddr_list[2] instead of
&statp->nsaddr_list[0].

Commit 3a85895fa3e1, which was reverted recently by commit cabba9343c8b,
trying to fix this by ignore EXT(statp).nsmap[], which is used to both
keep nameserver entries order and support option RES_ROTATE. But that
combine with wrong IPv4 nameserver entries count in statp->nscount
introduced by commit 16b7dc27a968 will cause new problem with RES_ROTATE
option on.

For example, if there are two nameserver entries in resolv.conf, and order
is IPv4 IPv6, then resolv/res_send.c, EXT(statp).nsmap[] will be 0 MAXNS+1
2. Without commit 3a85895fa3e1, this happens to work with rotate because
&statp->nsaddr_list[0] which is the first IPv4 nameserver address is also
copied to EXT(statp).nsaddrs[2]. With commit 3a85895fa3e1, &statp->nsaddr_
list[2] which doesn't exist is supposed to have been copied to EXT(statp).
nsaddrs[2] and L441 of resolv/res_send.c have allocated memory for it,
therefore will cause problem.

Even without commit 3a85895fa3e1, RES_ROTATE option still has problem
because of wrong IPv4 nameserver entries count in statp->nscount, which
is counted as nservall.

For example, if there is one and only one IPv6 nameserver entry in resolv.
conf, like BZ #13027, EXT(statp).nsmap[] will be MAXNS+1 0 MAXNS, then

resolv/res_init.c
@@ -308,9 +308,9 @@ __res_vinit(res_state statp, int preinit) {
 			cp++;
 		    if ((*cp != '\0') && (*cp != '\n')
 			&& __inet_aton(cp, &a)) {
-			statp->nsaddr_list[nservall].sin_addr = a;
-			statp->nsaddr_list[nservall].sin_family = AF_INET;
-			statp->nsaddr_list[nservall].sin_port =
+			statp->nsaddr_list[nserv].sin_addr = a;
+			statp->nsaddr_list[nserv].sin_family = AF_INET;
+			statp->nsaddr_list[nserv].sin_port =
 				htons(NAMESERVER_PORT);
 			nserv++;
 #ifdef _LIBC
@@ -414,7 +414,7 @@ __res_vinit(res_state statp, int preinit) {
 		    continue;
 		}
 	    }
-	    statp->nscount = nservall;
+	    statp->nscount = nserv;
 #ifdef _LIBC
 	    if (nservall - nserv > 0) {
 		statp->_u._ext.nscount6 = nservall - nserv;

resolv/res_send.c
@@ -408,7 +408,7 @@ __libc_res_nsend(res_state statp, const u_char *buf, int buflen,
 	 * Maybe initialize our private copy of the ns_addr_list.
 	 */
 	if (EXT(statp).nsinit == 0) {
-		unsigned char map[MAXNS];
+		u_int16_t map[MAXNS];
 
 		memset (map, MAXNS, sizeof (map));
 		for (n = 0; n < MAXNS; n++) {
@@ -447,7 +447,6 @@ __libc_res_nsend(res_state statp, const u_char *buf, int buflen,
 					sizeof (struct sockaddr_in6)
 					- sizeof (struct sockaddr_in));
 				EXT(statp).nssocks[n] = -1;
-				n++;
 			}
 		}
 		EXT(statp).nsinit = 1;